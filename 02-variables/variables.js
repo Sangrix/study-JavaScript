e96b72ad47d4b0d1a96ae50318d7bc6cde047e2a
ChoiMinSang <min393917@gmail.com>
2025-06-08 23:53:15 +0900

    ver0.2 file

---
+++ 02-variables/variables.js
@@ -0,0 +1,26 @@
+
+// 주석(comments): 실행되지 않는 코드, 이해를 돕기 위해 작성!
+
+// 엄격모드 적용!: 초창기 js의 단점을 보완하는 설정
+// (좀 더 엄격한 검증 적용!)
+'use srtict';
+
+// 1. 변수, 열린 상자 만들기!
+let openBox = "망고";
+console.log(openBox);
+
+openBox = "바둑이";
+console.log(openBox);
+
+// 2. 이름이 같은 변수, 생성 불가!
+// let openBox = "도넛";
+
+openBox = "도넛";
+console.log(openBox);
+
+// 3. 상수, 잠긴 상자 만들기!
+const cookieBox = "쿠키";
+console.log(cookieBox);
+
+// 상수의 값은 변경할 수 없음
+// cookieBox = "과일";
